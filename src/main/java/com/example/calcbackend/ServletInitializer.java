5f106c975629942afec68e7267c6d9fe4134f212
SaiKalyan2596 <anakinskywalker11021@gmail.com>
2025-08-20 09:39:54 +0530

    V1

---
+++ src/main/java/com/example/calcbackend/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.calcbackend;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CalcBackendApplication.class);
+    }
+
+}
---
+++ src/main/java/com/example/calcbackend/controller/ArithmeticOperations.java
@@ -0,0 +1,14 @@
+package com.example.calcbackend.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin("*")
+public class ArithmeticOperations {
+
+    @GetMapping("add/{A}/{B}")
+    public String add(@PathVariable("A") int a,@PathVariable("B") int b){
+        return "Addition = " + (a+b);
+    }
+}
